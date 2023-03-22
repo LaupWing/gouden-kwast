b69a66146acaa343ecd08a1b70567015cc4e78d5
LaupWing <loc_strict@hotmail.com>
2023-03-22 16:07:56 +0100

    temp links

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,41 @@
 import * as React from "react"
 
-const Layout = () => {
+const temp_links = [
+   {
+      to: "/",
+      name: "Home"
+   },
+   {
+      to: "/",
+      name: "Onze diensten"
+   },
+   {
+      to: "/",
+      name: "Portfolio"
+   },
+   {
+      to: "/",
+      name: "Vactures"
+   },
+   {
+      to: "/",
+      name: "Contact"
+   },
+]
+
+export default function Layout(){
    return (
-      <div>Layout</div>
+      <div className="w-screen h-screen flex flex-col">
+         <Header />
+      </div>
    )
 }
-export default Layout
+
+
+const Header = () => {
+   return (
+      <header className="w-full">
+
+      </header>
+   )
+}
