29299f8bdd1d7d8dc93299adc5c2246ce0d6d0b8
LaupWing <loc_strict@hotmail.com>
2023-03-23 10:54:46 +0100

    Request form

--- src/components/RequestForm.tsx
+++ src/components/RequestForm.tsx
@@ -2,9 +2,42 @@ import * as React from "react"
 
 const RequestForm = () => {
    return (
-      <form className="text-slate-700 flex flex-col max-w-md p-2">
+      <form className="text-slate-700 flex flex-col w-full max-w-xl p-2">
          <h2 className="text-2xl mb-4">Gratis adviesgesprek?</h2>
-         <input className="bg-white border-slate-500 rounded-[3px]" type="text" placeholder="Naam" />
+         <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-2 gap-4 col-span-2">
+               <input 
+                  className="bg-white border-gray-300 rounded-[3px]" 
+                  type="text" 
+                  placeholder="Jouw naam"
+               />
+               <input 
+                  className="bg-white border-gray-300 rounded-[3px]" 
+                  type="email" 
+                  placeholder="Jouw email" 
+               />
+            </div>
+            <div className="grid grid-cols-2 gap-4 col-span-2">
+               <input 
+                  className="bg-white border-gray-300 rounded-[3px]" 
+                  type="tel" 
+                  placeholder="Telefoon nummer"
+               />
+               <input 
+                  className="bg-white border-gray-300 rounded-[3px]" 
+                  type="text" 
+                  placeholder="Postcode" 
+               />
+            </div>
+            <textarea 
+               name="message" 
+               className="rounded-[3px] h-32 border-gray-300 resize-none col-span-2"
+               placeholder="Bericht"
+            ></textarea>
+            <button className="uppercase text-slate-600 font-bold text-sm tracking-wider bg-yellow-400 col-span-2 py-2 rounded-[3px]">
+               Aanvragen
+            </button>
+         </div>
       </form>
    )
 }
