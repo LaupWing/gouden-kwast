1a681f153ce1f6bda1046df2b1653d3e2fb764e3
LaupWing <loc_strict@hotmail.com>
2023-03-23 10:26:50 +0100

    udpate

--- src/components/Quotes.tsx
+++ src/components/Quotes.tsx
@@ -1,22 +1,28 @@
 import * as React from "react"
+import { AiOutlineFieldTime } from "react-icons/ai"
+import { BsShieldFillCheck } from "react-icons/bs"
+import { IoLocation } from "react-icons/io5"
 
 const Quotes = () => {
    return (
       <div className="grid grid-cols-3 text-white">
          <div className="aspect-[2/1] flex items-center justify-center bg-indigo-400">
-            <div className="max-w-[80%] space-y-4">
+            <div className="max-w-[80%] space-y-3 flex flex-col">
+               <AiOutlineFieldTime size={40}/>
                <h2 className="text-xl">Lorem ipsum</h2>
                <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam dolor. </p>
             </div>
          </div>
          <div className="aspect-[2/1] flex items-center justify-center bg-indigo-300">
-            <div className="max-w-[80%] space-y-4">
+            <div className="max-w-[80%] space-y-3 flex flex-col">
+               <BsShieldFillCheck size={38}/>
                <h2 className="text-xl">Lorem ipsum</h2>
                <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam dolor. </p>
             </div>
          </div>
          <div className="aspect-[2/1] flex items-center justify-center bg-indigo-400">
-            <div className="max-w-[80%] space-y-4">
+            <div className="max-w-[80%] space-y-3 flex flex-col">
+               <IoLocation size={40}/>
                <h2 className="text-xl">Lorem ipsum</h2>
                <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam dolor. </p>
             </div>
