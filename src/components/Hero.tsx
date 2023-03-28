38288d7a0736b2bcd9b71b3695a5d73c5140bc76
LaupWing <loc_strict@hotmail.com>
2023-03-28 10:44:28 +0200

    udpate

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,6 +5,7 @@ const Hero = () => {
    return (
       <>
          <HeroDesktop />
+         <HeroMobile />
       </>
    )
 }
@@ -13,7 +14,7 @@ export default Hero
 
 const HeroDesktop = () => {
    return (
-      <div className="h-minus-nav w-full flex">
+      <div className="h-minus-nav w-full lg:flex hidden">
          <div className="w-2/5 bg-slate-500 relative flex items-center justify-center">
             <div className="flex flex-col max-w-[80%] justify-center items-start space-y-10">
                <h2 className="text-4xl text-white font-semibold tracking-wide leading-[3rem]">Het schildersbedrijf voor regio Alkmaar</h2>
@@ -45,5 +46,25 @@ const HeroDesktop = () => {
 }
 
 const HeroMobile = () => {
-
+   return (
+      <div className="h-minus-nav w-full flex relative">
+         <div className="bg-slate-500 flex flex-col max-w-[80%] justify-center items-start space-y-10 z-50 p-4 my-auto rounded-md ml-4">
+            <h2 className="text-2xl text-white font-semibold tracking-wide leading-[2.5rem]">Het schildersbedrijf voor regio Alkmaar</h2>
+            <button className="flex shadow h-9">
+               <div className="bg-yellow-400 h-full flex items-center justify-center px-6 uppercase text-sm font-bold text-slate-600">
+                  Meer info
+               </div>
+               <div className="bg-slate-400 flex items-center justify-center px-2 text-white h-full flex-1">
+                  <IoArrowForwardCircle size={20}/>
+               </div>
+            </button>
+         </div>
+         <img
+            src="https://images.pexels.com/photos/4792523/pexels-photo-4792523.jpeg" 
+            className="w-full h-full object-cover absolute inset-0"
+            alt="" 
+         />
+         <div className="absolute inset-0 bg-slate-600/20"/>
+      </div>
+   )
 }
