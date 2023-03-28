5442a9e2d5412e4004dea8b082c0f79018912b65
LaupWing <loc_strict@hotmail.com>
2023-03-28 11:17:45 +0200

    ProjectType

---
+++ typings.d.ts
@@ -0,0 +1,12 @@
+export interface ProjectType {
+   project_id: string
+   project_title: string
+   client_name: string
+   address: any
+   project_description: string
+   type_painting: string[]
+   start_date: string
+   end_date: string
+   total_cost: Number
+   project_image: string
+}
