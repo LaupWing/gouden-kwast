ec088140b43677ea380be4d274f17c9407852a96
LaupWing <loc_strict@hotmail.com>
2023-03-31 12:52:45 +0200

    update

--- src/components/RecentWork.tsx
+++ src/components/RecentWork.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import data from "../dummy/projects.json"
+import clsx from "clsx"
 import { Carousel } from "react-responsive-carousel"
 
 export const RecentWork = () => {
@@ -9,6 +10,7 @@ export const RecentWork = () => {
             <h2 className="text-slate-100 text-3xl mb-4">Recente werk</h2>
             {/* <CarouselSmall /> */}
             <CarouselMedium />
+            <CarouselLarge />
             {/* <div className="grid lg:grid-cols-3 lg:gap-4 gap-y-10">
                {data.map((x, i) => (
                   <div 
@@ -71,12 +73,17 @@ const DynamicCarousel = ({
    className?: string
 }) => {
    const parts = Math.ceil(data.length / cardLength)
+   const styles = clsx(
+      "grid lg:gap-4 gap-y-10",
+      cardLength === 2 && "grid-cols-2", 
+      cardLength === 3 && "grid-cols-3", 
+   )
    
    return (
       <Carousel className={className} showThumbs={false} showStatus={false}>
          {[...Array(parts)].map((_, i) => (
             <div 
-               className="grid lg:grid-cols-2 lg:gap-4 gap-y-10"
+               className={styles}
                key={i}
             >
                {data
@@ -111,13 +118,15 @@ const CarouselMedium = () => {
    return (
       <DynamicCarousel 
          cardLength={2}
-         className=""
+         className="hidden md:block xl:hidden"
       />
    )
 }
 const CarouselLarge = () => {
    return (
-      <>
-      </>
+      <DynamicCarousel 
+         cardLength={3}
+         className="hidden xl:block"
+      />
    )
 }
