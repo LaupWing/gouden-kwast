f45602fdf56a93df58f778220ee5b2830257a9fa
LaupWing <loc_strict@hotmail.com>
2023-03-31 09:54:27 +0200

    udpate

--- src/components/Quotes.tsx
+++ src/components/Quotes.tsx
@@ -6,25 +6,25 @@ import { IoLocation } from "react-icons/io5"
 const Quotes = () => {
    return (
       <div className="grid lg:grid-cols-3 text-white">
-         <div className="aspect-[2/1] flex items-center justify-center bg-indigo-400">
-            <div className="max-w-[80%] space-y-3 flex flex-col">
-               <AiOutlineFieldTime size={40}/>
-               <h2 className="text-xl">Lorem ipsum</h2>
-               <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam dolor. </p>
+         <div className="aspect-[2/1] flex items-center justify-center py-10 bg-slate-500">
+            <div className="max-w-[80%] flex flex-col">
+               <AiOutlineFieldTime className="text-yellow-500" size={40}/>
+               <h2 className="text-xl text-yellow-400 mb-2">Lorem ipsum</h2>
+               <p className="text-sm"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam dolor. </p>
             </div>
          </div>
-         <div className="aspect-[2/1] flex items-center justify-center bg-indigo-300">
+         <div className="aspect-[2/1] flex items-center justify-center bg-slate-600">
             <div className="max-w-[80%] space-y-3 flex flex-col">
                <BsShieldFillCheck size={38}/>
                <h2 className="text-xl">Lorem ipsum</h2>
-               <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam dolor. </p>
+               <p className="text-sm"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam dolor. </p>
             </div>
          </div>
-         <div className="aspect-[2/1] flex items-center justify-center bg-indigo-400">
+         <div className="aspect-[2/1] flex items-center justify-center bg-slate-500">
             <div className="max-w-[80%] space-y-3 flex flex-col">
                <IoLocation size={40}/>
                <h2 className="text-xl">Lorem ipsum</h2>
-               <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam dolor. </p>
+               <p className="text-sm"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam dolor. </p>
             </div>
          </div>
       </div>
