2e5ae2c6e4c755e43668b5ea0c65495d7e93659b
LaupWing <loc_strict@hotmail.com>
2023-03-31 14:16:25 +0200

    finally

--- codegen.ts
+++ codegen.ts
@@ -1,15 +1,14 @@
-import type { CodegenConfig } from "@graphql-codegen/cli"
+
+import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-   overwrite: true,
-   schema: "https://goudenkwast.laupwing.site/graphql",
-   //   documents: "src/**/*.tsx",
-   generates: {
-      "src/generated": {
-         preset: "client",
-         plugins: [],
-      },
-   },
-}
+  overwrite: true,
+  schema: "https://goudenkwast.laupwing.site/graphql",
+  generates: {
+    "src/generated/graphql.ts": {
+      plugins: ["typescript"]
+    }
+  }
+};
 
-export default config
+export default config;
