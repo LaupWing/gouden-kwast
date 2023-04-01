07225ceef2d53540d71dbeee34d9bbdd881ab7a1
LaupWing <loc_strict@hotmail.com>
2023-04-01 07:52:12 +0200

    getMainMenu

---
+++ src/lib/utils.ts
@@ -0,0 +1,6 @@
+import { useMenuQuery } from "../hooks/useMenuQuery"
+
+export const getMainMenu = () => {
+   const allMenuItems = useMenuQuery()
+   return allMenuItems.filter(x => !x.parentId)
+}
