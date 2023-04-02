35e0301f561bbc6a9822e8ca3f7be27a0913371a
LaupWing <loc_strict@hotmail.com>
2023-04-02 18:06:12 +0200

    udpate

--- src/pages/onze-diensten.tsx
+++ src/pages/onze-diensten.tsx
@@ -12,12 +12,12 @@ const ContactPage: React.FC<PageProps<{
    console.log(data.allWpPage.nodes)
    return (
       <Layout>
-         <main className="flex-1 p-10 py-20 bg-slate-500">
+         <main className="flex-1 p-8 py-12 md:p-10 md:py-20 bg-slate-600">
             <h2 className="container mx-auto text-slate-100 text-3xl mb-4">Onze diensten</h2>
             <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 container mx-auto gap-4">
                {(data.allWpPage.nodes as Page[]).map(item => {
                   return (
-                     <div className="flex flex-col shadow bg-slate-700">
+                     <div className="flex flex-col shadow bg-slate-800">
                         <div className="relative">
                            <GatsbyImage 
                               // @ts-ignore
