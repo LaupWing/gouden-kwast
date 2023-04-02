c2321463b73f35d8f33c697a3b492ee5807a0dcd
LaupWing <loc_strict@hotmail.com>
2023-04-02 17:11:56 +0200

    udpate

---
+++ src/pages/contact.tsx
@@ -0,0 +1,38 @@
+import { HeadFC, PageProps, graphql } from "gatsby"
+import * as React from "react"
+import { Layout } from "../components"
+
+const ContactPage: React.FC<PageProps> = ({data, serverData}) => {
+   console.log(data)
+   return (
+      <Layout>
+         <main className="flex-1 pt-10">
+            Tessst
+         </main>
+      </Layout>
+   )
+}
+
+export default ContactPage
+
+export const Head: HeadFC = () => <title>Onze diensten</title>
+
+// export const pageQuery = graphql`
+//    query DienstenQuery {
+//       allWpPage(filter: {parentId: {eq: "cG9zdDo5OA=="}}) {
+//          nodes {
+//             parentId
+//             uri
+//             featuredImage {
+//                node {
+//                   gatsbyImage(placeholder: TRACED_SVG, width: 720)
+//                }
+//             }
+//             title
+//             onzeDiensten {
+//                description
+//             }
+//          }
+//       }
+//    }
+// `
