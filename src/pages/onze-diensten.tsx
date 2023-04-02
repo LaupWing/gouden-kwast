b59d05d8ad6cd677822b9afd180a4fed58984b70
LaupWing <loc_strict@hotmail.com>
2023-04-02 16:56:31 +0200

    update

--- src/pages/onze-diensten.tsx
+++ src/pages/onze-diensten.tsx
@@ -19,21 +19,20 @@ export const Head: HeadFC = () => <title>Onze diensten</title>
 
 export const pageQuery = graphql`
    query DienstenQuery {
-      allWpPage {
+      allWpPage(filter: {parentId: {eq: "cG9zdDo5OA=="}}) {
          nodes {
             parentId
             uri
             featuredImage {
-            node {
-               gatsbyImage(placeholder: TRACED_SVG, width: 720)
-            }
+               node {
+                  gatsbyImage(placeholder: TRACED_SVG, width: 720)
+               }
             }
             title
             onzeDiensten {
-            description
+               description
             }
          }
       }
-      }
-
+   }
 `
