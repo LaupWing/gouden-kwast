179f8dd553b46f292ed386d2b135c897e6b97d41
LaupWing <loc_strict@hotmail.com>
2023-04-03 08:20:29 +0200

    fixed the image

--- src/pages/onze-diensten/{wpPage.slug}.tsx
+++ src/pages/onze-diensten/{wpPage.slug}.tsx
@@ -1,11 +1,36 @@
 import * as React from "react"
 import { Layout } from "~/components"
+import { graphql, PageProps } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-const OnzeDienstenDetail = () => {
+const OnzeDienstenDetail: React.FC<PageProps> = ({ data }) => {
+   console.log(data)
    return (
       <Layout>
-         
+         <main className="flex-1 bg-slate-500">
+            <GatsbyImage
+               // @ts-ignore
+               image={getImage(data.wpPage.featuredImage?.node.gatsbyImage)!}
+               // @ts-ignore
+               alt={data.wpPage.title!}
+               className="w-full object-cover max-h-80 h-[30vh]"
+            />
+         </main>
       </Layout>
    )
 }
 export default OnzeDienstenDetail
+
+export const pageQuery = graphql`
+   query OnzeDienstenDetail($id: String!) {
+      wpPage(id: {eq: $id}) {
+         content
+         title
+         featuredImage {
+            node {
+               gatsbyImage(placeholder: TRACED_SVG, width: 720)
+            }
+         }
+      }
+   }
+`
