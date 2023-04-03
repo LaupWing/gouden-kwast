dd746e4fe23b9d46297a0da9b69c31bed74075de
LaupWing <loc_strict@hotmail.com>
2023-04-03 08:22:25 +0200

    added page props

--- src/pages/onze-diensten/{wpPage.slug}.tsx
+++ src/pages/onze-diensten/{wpPage.slug}.tsx
@@ -2,18 +2,20 @@ import * as React from "react"
 import { Layout } from "~/components"
 import { graphql, PageProps } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { Page } from "~/generated/graphql"
 
-const OnzeDienstenDetail: React.FC<PageProps> = ({ data }) => {
-   console.log(data)
+const OnzeDienstenDetail: React.FC<PageProps<{
+   wpPage: Page 
+}>> = ({ data }) => {
+   
    return (
       <Layout>
-         <main className="flex-1 bg-slate-500">
+         <main className="flex-1 bg-slate-200">
             <GatsbyImage
                // @ts-ignore
                image={getImage(data.wpPage.featuredImage?.node.gatsbyImage)!}
-               // @ts-ignore
                alt={data.wpPage.title!}
-               className="w-full object-cover max-h-80 h-[30vh]"
+               className="w-full object-cover max-h-60 h-[30vh]"
             />
          </main>
       </Layout>
