ac9c1013f315de0d75f8f5d499b7848d022db92b
LaupWing <loc_strict@hotmail.com>
2023-04-03 08:26:31 +0200

    header

--- src/pages/onze-diensten/{wpPage.slug}.tsx
+++ src/pages/onze-diensten/{wpPage.slug}.tsx
@@ -11,12 +11,20 @@ const OnzeDienstenDetail: React.FC<PageProps<{
    return (
       <Layout>
          <main className="flex-1 bg-slate-200">
-            <GatsbyImage
-               // @ts-ignore
-               image={getImage(data.wpPage.featuredImage?.node.gatsbyImage)!}
-               alt={data.wpPage.title!}
-               className="w-full object-cover max-h-60 h-[30vh]"
-            />
+            <div className="relative flex">
+               <div className="absolute inset-0 bg-slate-200/40 z-50"/>
+               <div className="absolute z-[100] flex justify-center items-center inset-0">
+                  <h2 className="uppercase font-display font-bold text-yellow-400 tracking-wider bg-slate-800/70 text-xl backdrop-blur px-8 py-3 rounded shadow">
+                     {data.wpPage.title!}
+                  </h2>
+               </div>
+               <GatsbyImage
+                  // @ts-ignore
+                  image={getImage(data.wpPage.featuredImage?.node.gatsbyImage)!}
+                  alt={data.wpPage.title!}
+                  className="w-full object-cover max-h-60 h-[30vh]"
+               />
+            </div>
          </main>
       </Layout>
    )
