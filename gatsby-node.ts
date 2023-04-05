3bebc8ca204118c23124227fc5f8b0acf8f5ab1f
LaupWing <loc_strict@hotmail.com>
2023-04-05 08:24:37 +0200

    udpate

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,19 +1,33 @@
 import { GatsbyNode } from "gatsby"
+import { RootQuery } from "./src/generated/graphql"
 
 export const createPages: GatsbyNode["createPages"] = async ({
    actions,
-   graphql
+   graphql,
+   reporter
 }) => {
-   const test = await graphql(`
+   const resultBlogs = await graphql<{
+      wp: RootQuery
+      allWpPost: {
+         totalCount: number
+      }
+   }>(`#graphql
       query {
          wp {
             readingSettings {
                postsPerPage
             }
          }  
+         allWpPost {
+            totalCount
+         }
       }
    `)
 
-   console.log(test)
+   if(resultBlogs.errors){
+      reporter.panicOnBuild("Something went wrong!", resultBlogs.errors)
+   }
+   console.log(Math.ceil(resultBlogs.data!.allWpPost!.totalCount / resultBlogs.data!.wp!.readingSettings!.postsPerPage!))
+   console.log(resultBlogs)
    console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
 }
