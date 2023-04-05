3b7a784755875cd2c4394a5a2cb75a2e9661cc1a
LaupWing <loc_strict@hotmail.com>
2023-04-05 07:55:32 +0200

    udpate

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,7 +1,7 @@
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import * as React from "react"
 import type { FC } from "react"
-import { Post } from "~/generated/graphql"
+import { Post, Tag } from "~/generated/graphql"
 
 export const BlogCard:FC<{
    blog: Post
@@ -9,6 +9,11 @@ export const BlogCard:FC<{
    return (
       <div className="flex flex-col text-left">
          <div className="aspect-[4/3] relative">
+            <div className="absolute top-2 left-2 z-50 flex gap-3 tracking-wider font-bold uppercase text-sm">
+               {blog.tags?.nodes.map((tag: Tag) => (
+                  <div className="px-2 py-0.5 rounded bg-slate-700 text-yellow-400">{tag.name}</div>
+               ))}
+            </div>
             <GatsbyImage
                // @ts-ignore
                image={getImage(blog.featuredImage?.node.localFile.childImageSharp.gatsbyImageData)!}
