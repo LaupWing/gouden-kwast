40bb1d0e1976d43b5a1033e44ceaec3dce7aa73d
LaupWing <loc_strict@hotmail.com>
2023-04-05 07:48:32 +0200

    containerbanner content centered

--- src/components/ContactBanner.tsx
+++ src/components/ContactBanner.tsx
@@ -3,7 +3,7 @@ import { IoArrowForwardCircle } from "react-icons/io5"
 
 export const ContactBanner = () => {
    return (
-      <div className="bg-slate-700">
+      <div className="bg-slate-700 flex items-center justify-center">
          <div className="container py-10 px-6 lg:px-0 flex lg:flex-row flex-col justify-between lg:items-center items-start">
             <h2 className="text-2xl font-bold text-slate-200">Geïnteresseerd?</h2>
             <div className="flex text-yellow-400 uppercase tracking-wider items-center font-bold text-sm">
