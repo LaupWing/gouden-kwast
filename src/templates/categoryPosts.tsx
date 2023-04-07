668cbdea2b49a5b4bf120ab51a779e8f92ba4fd9
LaupWing <loc_strict@hotmail.com>
2023-04-07 12:07:34 +0200

    udpate

--- src/templates/categoryPosts.tsx
+++ src/templates/categoryPosts.tsx
@@ -1,8 +1,7 @@
 import { HeadFC, PageProps, graphql } from "gatsby"
 import * as React from "react"
-import { BlogCard, CategoriesMenu, CategorySection, ContactBanner, Pagination } from "~/components"
+import { CategorySection } from "~/components"
 import { CategoryConnectionEdge, PostConnection } from "~/generated/graphql"
-import { motion } from "framer-motion"
 
 const PostsPage: React.FC<PageProps<{
    allWpPost: PostConnection
@@ -12,35 +11,14 @@ const PostsPage: React.FC<PageProps<{
    currentPage: number
    categoryUri: string
 }>> = ({data, pageContext}) => {
-   const container = {
-      hidden: {},
-      show: {
-         transition: {
-            staggerChildren: 0.1,
-            delayChildren: 0.6,
-         },
-      },
-   }
-   const item = {
-      hidden: { 
-         opacity: 0,
-         y: 20
-      },
-      show: { 
-         opacity: 1,
-         y: 0 
-      },
-   }
    return (
-      <>
-         <CategorySection 
-            posts={data.allWpPost}
-            categories={pageContext.categories}
-            numberOfPages={pageContext.numberOfPages}
-            categoryUri={pageContext.categoryUri}
-            currentPage={pageContext.currentPage}
-         />
-      </>
+      <CategorySection 
+         posts={data.allWpPost}
+         categories={pageContext.categories}
+         numberOfPages={pageContext.numberOfPages}
+         categoryUri={pageContext.categoryUri}
+         currentPage={pageContext.currentPage}
+      />
    )
 }
 
--- src/templates/posts.tsx
+++ src/templates/posts.tsx
@@ -1,6 +1,6 @@
 import { HeadFC, Link, PageProps, graphql } from "gatsby"
 import * as React from "react"
-import { BlogCard, CategoriesMenu, ContactBanner, Pagination } from "~/components"
+import { BlogCard, CategoriesMenu, CategorySection, ContactBanner, Pagination } from "~/components"
 import { CategoryConnectionEdge, PostConnection } from "~/generated/graphql"
 
 const PostsPage: React.FC<PageProps<{
@@ -12,30 +12,13 @@ const PostsPage: React.FC<PageProps<{
    categoryUri: string
 }>> = ({data, pageContext}) => {
    return (
-      <>
-         <main className="flex-1 p-8 py-12 md:p-10 md:py-20 bg-slate-800">
-            <h2 className="container mx-auto text-slate-100 text-3xl mb-4">Portfolio</h2>
-            <div className="flex items-start gap-4 container mx-auto">
-               <section className="grid grid-cols-2 gap-6 container m-auto">
-                  {data.allWpPost.nodes.map(blog => (
-                     <BlogCard 
-                        blog={blog}
-                        key={blog.id}
-                     />
-                  ))}
-                  <Pagination
-                     currentPage={pageContext.currentPage}
-                     numberOfPages={pageContext.numberOfPages}
-                     categoryUri={pageContext.categoryUri}
-                  />
-               </section>
-               <CategoriesMenu 
-                  categories={pageContext.categories}
-               />
-            </div>
-         </main>
-         <ContactBanner />
-      </>
+      <CategorySection
+         posts={data.allWpPost}
+         categories={pageContext.categories}
+         numberOfPages={pageContext.numberOfPages}
+         categoryUri={pageContext.categoryUri}
+         currentPage={pageContext.currentPage}
+      />
    )
 }
 
