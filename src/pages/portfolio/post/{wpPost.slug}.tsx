ff83251024820ad0d4e5e30c78da4871d182345f
LaupWing <loc_strict@hotmail.com>
2023-04-11 16:04:02 +0200

    udpate

--- src/pages/portfolio/post/{wpPost.slug}.tsx
+++ src/pages/portfolio/post/{wpPost.slug}.tsx
@@ -3,6 +3,7 @@ import { ContactBanner } from "~/components"
 import { graphql, PageProps } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { Post } from "~/generated/graphql"
+import { Category } from "~/generated/graphql"
 
 const PostDetailPage: React.FC<PageProps<{
    wpPost: Post
@@ -13,9 +14,21 @@ const PostDetailPage: React.FC<PageProps<{
             <div className="relative flex">
                <div className="absolute inset-0 bg-slate-200/40 z-50"/>
                <div className="absolute z-[100] flex justify-center items-center inset-0">
-                  <h2 className="uppercase font-display font-bold text-yellow-400 tracking-wider bg-slate-800/70 text-xl backdrop-blur px-8 py-3 rounded shadow">
-                     {data.wpPost.title!}
-                  </h2>
+                  <div className="flex flex-col items-center">
+                     <h2 className="uppercase font-display font-bold text-yellow-400 tracking-wider bg-slate-800/70 text-xl backdrop-blur px-8 py-3 rounded shadow">
+                        {data.wpPost.title!}
+                     </h2>
+                     <div className="mt-2 flex gap-3 tracking-wider font-bold uppercase text-sm flex-wrap">
+                        {data.wpPost.categories?.nodes.map((category: Category) => (
+                           <div 
+                              className="px-3 py-1 rounded bg-slate-700/60 backdrop-blur text-yellow-400"
+                              key={category.id}
+                           >
+                              {category.name}
+                           </div>
+                        ))}
+                     </div>
+                  </div>
                </div>
                <GatsbyImage
                   // @ts-ignore
