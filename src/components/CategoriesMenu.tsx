6d3dca4522c09461f7bc34a1523a1d308d7e7d6f
LaupWing <loc_strict@hotmail.com>
2023-04-12 09:57:34 +0200

    update

--- src/components/CategoriesMenu.tsx
+++ src/components/CategoriesMenu.tsx
@@ -10,7 +10,6 @@ export const CategoriesMenu:React.FC<{
 }) => {
    return (
       <>
-         {/* <HiOutlineMenuAlt1 size={26} className="text-white" /> */}
          <CategoriesMenuDesktop 
             categories={categories} 
          />
@@ -63,34 +62,37 @@ const CategoriesMenuMobile:React.FC<{
 }> = ({
    categories
 }) => (
-   <ul className="bg-slate-50 w-60 md:hidden flex flex-col flex-shrink-0 rounded py-4 px-8 absolute right-0">
-      <li className="uppercase font-bold text-xs tracking-widest text-slate-400">
-         <h2>Categoriën</h2>
-      </li>
-      <div className="flex flex-col gap-2 my-4">
-         <Link 
-            to="/portfolio"
-            className="text-slate-500/50 hover:text-slate-500/80 duration-500"
-            activeClassName="!text-slate-500"
-         >
-            <li>Alle</li>
-         </Link>
-         {categories.map(category => (
+   <>
+      <HiOutlineMenuAlt1 size={26} className="text-white" />
+      <ul className="bg-slate-50 w-60 md:hidden hidden flex-col flex-shrink-0 rounded py-4 px-8 absolute right-0">
+         <li className="uppercase font-bold text-xs tracking-widest text-slate-400">
+            <h2>Categoriën</h2>
+         </li>
+         <div className="flex flex-col gap-2 my-4">
             <Link 
-               to={category.node.uri!}
+               to="/portfolio"
                className="text-slate-500/50 hover:text-slate-500/80 duration-500"
                activeClassName="!text-slate-500"
-               partiallyActive
-               key={category.node.id}
             >
-               <li
-                  className=""
+               <li>Alle</li>
+            </Link>
+            {categories.map(category => (
+               <Link 
+                  to={category.node.uri!}
+                  className="text-slate-500/50 hover:text-slate-500/80 duration-500"
+                  activeClassName="!text-slate-500"
+                  partiallyActive
                   key={category.node.id}
                >
-                  {category.node.name}
-               </li>
-            </Link>
-         ))}
-      </div>
-   </ul>
+                  <li
+                     className=""
+                     key={category.node.id}
+                  >
+                     {category.node.name}
+                  </li>
+               </Link>
+            ))}
+         </div>
+      </ul>
+   </>
 )
