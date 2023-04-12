35c03d40a1faabc64db471e9a7b321df4c001e9d
LaupWing <loc_strict@hotmail.com>
2023-04-12 10:30:15 +0200

    udpate

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,14 +1,29 @@
 import { HeadFC, PageProps } from "gatsby"
 import * as React from "react"
-import { ContactBanner } from "~/components"
+import { ContactBanner, IconSearch } from "~/components"
 
 const SearchPage: React.FC<PageProps> = () => {
+   // const onSubmit = (e) => {
+
+   // }
+
    return (
       <>
          <main className="flex-1 p-8 py-12 md:p-10 md:py-20 bg-slate-600">
-            <div className="flex">
-               <input type="text" />
-            </div>
+            <form 
+               className="flex rounded overflow-hidden">
+               <input 
+                  type="text" 
+                  className="py-1 flex-1 border-none" 
+                  placeholder="Zoeken naar"
+               />
+               <button 
+                  className="bg-yellow-400 py-2 w-16 flex items-center justify-center text-slate-800"
+                  type="submit"
+               >
+                  <IconSearch size={22}/>
+               </button>
+            </form>
          </main>
          <ContactBanner />
       </>
