ca271731ba8ec59ff815b62e82bee16ac9049ac3
LaupWing <loc_strict@hotmail.com>
2023-04-12 09:50:16 +0200

    categories desktop

--- src/components/CategoriesMenu.tsx
+++ src/components/CategoriesMenu.tsx
@@ -11,36 +11,46 @@ export const CategoriesMenu:React.FC<{
    return (
       <>
          {/* <HiOutlineMenuAlt1 size={26} className="text-white" /> */}
-         <ul className="bg-slate-50 w-60 flex-shrink-0 rounded py-4 px-8">
-            <li className="uppercase font-bold text-xs tracking-widest text-slate-400">
-               <h2>Categoriën</h2>
-            </li>
-            <div className="flex flex-col gap-2 my-4">
-               <Link 
-                  to="/portfolio"
-                  className="text-slate-500/50 hover:text-slate-500/80 duration-500"
-                  activeClassName="!text-slate-500"
-               >
-                  <li>Alle</li>
-               </Link>
-               {categories.map(category => (
-                  <Link 
-                     to={category.node.uri!}
-                     className="text-slate-500/50 hover:text-slate-500/80 duration-500"
-                     activeClassName="!text-slate-500"
-                     partiallyActive
-                     key={category.node.id}
-                  >
-                     <li
-                        className=""
-                        key={category.node.id}
-                     >
-                        {category.node.name}
-                     </li>
-                  </Link>
-               ))}
-            </div>
-         </ul>
+         <CategoriesMenuDesktop 
+            categories={categories} 
+         />
       </>
    )
 }
+
+const CategoriesMenuDesktop:React.FC<{
+   categories:  CategoryConnectionEdge[]
+}> = ({
+   categories
+}) => (
+   <ul className="bg-slate-50 w-60 hidden md:flex flex-col flex-shrink-0 rounded py-4 px-8">
+      <li className="uppercase font-bold text-xs tracking-widest text-slate-400">
+         <h2>Categoriën</h2>
+      </li>
+      <div className="flex flex-col gap-2 my-4">
+         <Link 
+            to="/portfolio"
+            className="text-slate-500/50 hover:text-slate-500/80 duration-500"
+            activeClassName="!text-slate-500"
+         >
+            <li>Alle</li>
+         </Link>
+         {categories.map(category => (
+            <Link 
+               to={category.node.uri!}
+               className="text-slate-500/50 hover:text-slate-500/80 duration-500"
+               activeClassName="!text-slate-500"
+               partiallyActive
+               key={category.node.id}
+            >
+               <li
+                  className=""
+                  key={category.node.id}
+               >
+                  {category.node.name}
+               </li>
+            </Link>
+         ))}
+      </div>
+   </ul>
+)
